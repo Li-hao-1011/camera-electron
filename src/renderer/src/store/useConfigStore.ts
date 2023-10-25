bc7e346c52cddadcaec82423f157d747ca5db890
lihao <lihao.coder.foxmail.com>
2023-10-25 23:22:26 +0800

    使用pinia持久化

---
+++ src/renderer/src/store/useConfigStore.ts
@@ -0,0 +1,30 @@
+// stores/counter.js
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { Page } from './types'
+
+type ConfigType = {
+  deviceId: string
+  page: Page
+  borderWidth: string
+  borderColor: string
+  rounded: boolean
+}
+
+export const useConfigStore = defineStore(
+  'config',
+  () => {
+    const config = ref<ConfigType>({
+      deviceId: '',
+      page: Page.Setting,
+      borderWidth: '1px',
+      borderColor: 'pink',
+      rounded: false
+    })
+
+    return { config }
+  },
+  {
+    persist: true // 本地持久化
+  }
+)

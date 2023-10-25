30c24755ddb8e29b950260b9dbfc6076b9df39c8
lihao <lihao.coder.foxmail.com>
2023-10-25 23:22:58 +0800

    配置主页面

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Camera from './components/Camera.vue'
+import { SettingOne, CameraOne, InnerShadowTopLeft, Square } from '@icon-park/vue-next'
+import Setting from './components/Setting.vue'
+// import type { Page } from './hooks/useSetting'
+// import useSetting from './hooks/useSetting'
+import { useConfigStore } from '@renderer/store/useConfigStore'
+import { Page } from '@renderer/store/types'
+
+const { config } = useConfigStore()
+const quit = () => {
+  window.api.quit()
+}
+</script>
 
 <template>
-  <div>My electron</div>
-  <el-button type="primary">使用element-plus</el-button>
-  <div class="bg-red-600">53454352342</div>
-</template>
+  <Suspense>
+    <main class="relative group" @contextmenu="quit">
+      <section>
+        <SettingOne
+          v-if="config.page === Page.Camera"
+          theme="outline"
+          size="24"
+          class="setting-icon text-black opacity-80 absolute left-1/2 -translate-x-1/2 mt-3 cursor-pointer z-10 hidden group-hover:block"
+          @click="config.page = Page.Setting"
+        />
+        <CameraOne
+          v-if="config.page === Page.Setting"
+          theme="outline"
+          size="24"
+          class="setting-icon text-white opacity-80 absolute left-1/2 -translate-x-1/2 mt-3 cursor-pointer z-10"
+          @click="config.page = Page.Camera"
+        />
 
-<style lang="less"></style>
+        <div
+          v-show="config.page === Page.Camera"
+          class="setting-icon text-black opacity-80 absolute left-1/2 -translate-x-1/2 bottom-3 mt-3 cursor-pointer z-10"
+          @click="config.rounded = !config.rounded"
+        >
+          <Square v-if="config.rounded" theme="outline" size="24" />
+          <InnerShadowTopLeft v-else theme="outline" size="24" />
+        </div>
+      </section>
+      <section>
+        <Camera v-if="config.page === Page.Camera" />
+        <Setting v-else-if="config.page === Page.Setting" />
+        <div v-else></div>
+      </section>
+    </main>
+  </Suspense>
+</template>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/css/tialwind.css'
+import './assets/css/global.scss'
 
-createApp(App).use(ElementPlus).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(ElementPlus).use(pinia).mount('#app')
